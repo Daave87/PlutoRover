50754177828476bee775176cf5a6b7c7f0244f93
Daave <Daave@DESKTOP-TM5J01O>
2018-01-28 22:00:14 +0000

    Initial commit and first test

---
+++ PlutoRover/PlutoRover.Tests/RoverTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace PlutoRover.Tests
+{
+    [TestFixture]
+    public class RoverTests
+    {
+        [Test]
+        public void ForwardCommandWhenFacingNorthIncreasesYByOne()
+        {
+            //arrange
+            var rover = new Rover();
+
+            //act
+            rover.Go("F");
+
+            //assert
+            Assert.That(rover.x == 0);
+            Assert.That(rover.y == 1);
+            Assert.That(rover.Direction == "N");
+        }
+    }
+}
---
+++ PlutoRover/PlutoRover/Rover.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlutoRover
+{
+    public class Rover
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public string Direction { get; set; }
+
+        public void Go(string command)
+        {
+            if (Direction == "N" && command == "F")
+            {
+                y += 1;
+            }
+        }
+
+        public Rover()
+        {
+            x = 0;
+            y = 0;
+            Direction = "N";
+        }
+    }
+}
