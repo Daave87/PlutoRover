b66a67f41f224ad6d911c2bb9ce0bf4dd779164a
Daave <Daave@DESKTOP-TM5J01O>
2018-01-28 22:05:39 +0000

    2

--- PlutoRover/PlutoRover.Tests/RoverTests.cs
+++ PlutoRover/PlutoRover.Tests/RoverTests.cs
@@ -19,5 +19,21 @@ namespace PlutoRover.Tests
             Assert.That(rover.y == 1);
             Assert.That(rover.Direction == "N");
         }
+        
+        [Test]
+        public void ForwardCommandTwiceWhenFacingNorthIncreasesYByTwo()
+        {
+            //arrange
+            var rover = new Rover();
+
+            //act
+            rover.Go("F");
+            rover.Go("F");
+            
+            //assert
+            Assert.That(rover.x == 0);
+            Assert.That(rover.y == 2);
+            Assert.That(rover.Direction == "N");
+        }
     }
 }
